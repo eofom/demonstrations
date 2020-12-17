9c9502f0125b5d1c199f66a3b7ad96f5361e36e0
Egor Fo <eeofom@gmail.com>
2020-12-17 11:50:26 +0300

    stack speed demonstration is now c++17 and avg times are used

--- memory_speed.cpp
+++ memory_speed.cpp
@@ -1,15 +1,21 @@
 #include <chrono>
 #include <algorithm>
 #include <iostream>
-#include <numeric>
 #include <vector>
 #include <stack>
 #include <random>
 
+#undef NDEBUG
+#include <cassert>
+
+constexpr double kPopMarker = 0.9834111245;
+
 template<typename Func, typename ...Args>
-double timeExecution(Func func, Args ...args) {
+double timeExecution(int iterations, Func func, Args ...args) {
     auto start = std::chrono::high_resolution_clock::now();
-    func(args...);
+    for (int i = 0; i < iterations; ++i) {
+        func(args...);
+    }
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
     return duration.count();
@@ -33,6 +39,10 @@ struct PairMinStack {
         return res;
     }
 
+    bool empty() const {
+        return stack.empty();
+    }
+
     std::stack<MinPair> stack;
 };
 
@@ -50,47 +60,123 @@ struct DoubleStackMinStack {
         return res;
     }
 
+    bool empty() const {
+        return vals.empty();
+    }
+
     std::stack<double> vals;
     std::stack<double> mins;
+};
+
+struct OptimizedMinStack {
+    void push(double val) {
+        double min = vals.empty() ? val : std::min(val, mins.top());
+        vals.emplace(val);
+        if (min == val) {
+            mins.emplace(min);
+        }
+    }
+
+    MinPair pop() {
+        MinPair res{vals.top(), mins.top()};
+        if (vals.top() == mins.top()) {
+            mins.pop();
+        }
+        vals.pop();
+        return res;
+    }
+
+    bool empty() const {
+        return vals.empty();
+    }
+
+    std::stack<double> vals;
+    std::stack<double> mins;
+};
+
+template<typename Stack>
+void testMinStackAgainstPairMinStack(const std::vector<double> &vals, int seed) {
+    std::mt19937 generator(seed);
+    Stack testedStack;
+    PairMinStack referenceStack;
+    int valIndex = 0;
+    while (valIndex < vals.size() && !referenceStack.empty()) {
+        bool canPop = !referenceStack.empty();
+        bool canPush = valIndex < vals.size();
+        bool shouldPush =
+                !canPop || canPush && static_cast<bool>(std::uniform_int_distribution<int>(0, 1)(generator));
+        if (shouldPush) {
+            testedStack.push(vals.at(valIndex));
+            referenceStack.push(vals.at(valIndex));
+            ++valIndex;
+        } else {
+            auto testedPair = testedStack.pop();
+            auto referencePair = referenceStack.pop();
+            assert(testedPair.val == referencePair.val);
+            assert(testedPair.min == referencePair.min);
+        }
+    }
+}
+
+struct FakeMinStack {
+    void push(double val) {
+        stack.emplace(val);
+    }
+
+    MinPair pop() {
+        auto res = stack.top();
+        stack.pop();
+        return {res, 0.};
+    }
+
+    bool empty() const {
+        return stack.empty();
+    }
+
+    std::stack<double> stack;
 };
 
 template <typename Stack>
 void testStack(const std::vector<double> &vals) {
     Stack stack;
-    for (double val : vals) {
-        stack.push(val);
-    }
-    double min = std::numeric_limits<double>::min();
-    for (auto it = vals.rbegin(); it != vals.rend(); ++it) {
-        auto minPair = stack.pop();
-        if (minPair.min < min || minPair.val != *it) {
-            throw std::logic_error("vals/stack incostistence");
+    for (const auto &val : vals) {
+        if (val == kPopMarker) {
+            if (!stack.empty()) {
+                stack.pop();
+            }
         }
-        min = minPair.min;
+        stack.push(val);
     }
 }
 
 int main() {
-    std::mt19937 generator{std::random_device{}()};
+    std::random_device trueRandomGenerator{};
+    std::mt19937 generator{trueRandomGenerator()};
 
     std::vector<double> vals;
     int testSize = 1'000'000;
+    double popProbability = 0.5;
+    std::discrete_distribution<int> popDistribution({1 - popProbability, popProbability});
+    std::uniform_real_distribution<double> pushDistribution(-1., 1.);
     for (int i = 0; i < testSize; ++i) {
-        vals.emplace_back(generator());
-    }
-    int testCount = 100;
-
-    double totalDoubleStackTime = 0.;
-    for (int i = 0; i < testCount; ++i) {
-        auto doubleStackTime = timeExecution(testStack<DoubleStackMinStack>, vals);
-        totalDoubleStackTime += doubleStackTime;
+        if (popDistribution(generator)) {
+            vals.emplace_back(kPopMarker);
+        } else {
+            vals.emplace_back(pushDistribution(generator));
+        }
     }
 
-    double totalPairTime = 0;
-    for (int i = 0; i < testCount; ++i) {
-        auto pairTime = timeExecution(testStack<PairMinStack>, vals);
-        totalPairTime += pairTime;
-    }
-    std::cout << "pair time: " << totalPairTime << "s\n";
-    std::cout << "double stack time: " << totalDoubleStackTime << "s\n";
+    testMinStackAgainstPairMinStack<OptimizedMinStack>(vals, trueRandomGenerator());
+    testMinStackAgainstPairMinStack<DoubleStackMinStack>(vals, trueRandomGenerator());
+
+    int testIterations = 100;
+    std::cout << "PairMinStack time: " << timeExecution(testIterations, testStack<PairMinStack>, vals) << "s\n";
+    std::cout << "DoubleStackMinStack time: " << timeExecution(testIterations, testStack<DoubleStackMinStack>, vals) << "s\n";
+    std::cout << "OptimizedMinStack time: " << timeExecution(testIterations, testStack<OptimizedMinStack>, vals) << "s\n";
+    std::cout << "FakeMinStack time: " << timeExecution(testIterations, testStack<FakeMinStack>, vals) << "s\n";
 }
+
+//PairMinStack time: 144.286s
+//DoubleStackMinStack time: 187.129s
+//OptimizedMinStack time: 99.6452s
+//FakeMinStack time: 86.0847s
